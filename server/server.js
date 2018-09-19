b1d9d659b0f110332ecf38f11fe6afdd515115e2
David <dme280@nyu.edu>
2018-09-18 21:35:28 -0400

    greet new users, alert others

--- server/server.js
+++ server/server.js
@@ -15,6 +15,17 @@ app.use(express.static(PublicPath));
 io.on("connection", (socket) => {
     console.log("New user connected");
 
+    socket.emit('newMessage', {
+        from: 'admin',
+        text: 'Welcome to the app'
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user has joined!',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (message) => {
         io.emit('newMessage', {
             from: message.from,
@@ -22,6 +33,13 @@ io.on("connection", (socket) => {
             createdAt: new Date().getTime()
         });
     });
+
+    //     socket.broadcast.emit('newMessage', {
+    //         from: message.from,
+    //         text: message.text,
+    //         createdAt: new Date().getTime()
+    //     });
+    // });
 
     socket.on("disconnect", () => {
         console.log("Disconnected from server");
